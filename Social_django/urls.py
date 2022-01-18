90b2bae8ba473071ed2e0e0be89a1464dc6723d7
s248h176 <s248h176@gmail.com>
2022-01-18 10:39:02 -0600

    Another One

--- Social_django/urls.py
+++ Social_django/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
+from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
