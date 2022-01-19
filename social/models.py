11d73d78a81236cbaba1feedb30c0c93a1c9ccd3
XuyenHoang <xhoang21@gmail.com>
2022-01-19 11:07:12 -0500

    likes test

--- social/models.py
+++ social/models.py
@@ -2,6 +2,8 @@ from datetime import date
 from django.db import models
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFit
+from Social_django.settings import AUTH_USER_MODEL 
+
 
 
 
@@ -9,6 +11,7 @@ class Photo(models.Model):
     # user = models.ForeignKey( User, verbose_name="Created By", on_delete=models.CASCADE, related_name='photos')
     description = models.CharField(max_length=1000)
     photo = ProcessedImageField(upload_to="photos", format="JPEG", options={"quality": 80}, processors=[ResizeToFit(width=1000, height=1000)])
+    # likes = models.ManyToManyField(AUTH_USER_MODEL, related_name='photo')
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
