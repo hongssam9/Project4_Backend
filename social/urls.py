72583494a4d65ff9f2378f8c89b1e33a14593204
tadsmyth <tadsmith7@gmail.com>
2022-01-23 14:30:53 -0600

    front/bacck connected, MAYBE

--- social/urls.py
+++ social/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.PhotoList.as_view(), name='photo_list'),
+    # temp disabling this url to test react-integrations - Tad
+    # path('', views.PhotoList.as_view(), name='photo_list'),
     path('photo/', views.PhotoList.as_view(), name='photo_list'),
     path('photo/<int:pk>', views.PhotoDetail,name='photo_detail'),
     path('comment/', views.CommentList.as_view(), name='comment_list'),
     path('comment/<int:pk>', views.CommentDetail.as_view(),name='comment_detail'),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
